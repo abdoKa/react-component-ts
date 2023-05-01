2693b8ddfdf4695fc3a22ee869607b7d003d6e02
Abdelali Kabou <abdelali.kabou@gmail.com>
2023-05-01 06:58:32 +0100

    feat(multi-select): finalize multi select input

--- multi-select-input/src/App.tsx
+++ multi-select-input/src/App.tsx
@@ -1,16 +1,26 @@
+import { useState } from "react"
+import { SelectOption } from "./components/MultiSelect"
 import MultiSelect from "./components/MultiSelect"
 
-function App() {
-  const options = [{
-    label: 'React',
-    value: 'react'
-  }]
+const options = [
+  { label: "First", value: 1 },
+  { label: "Second", value: 2 },
+  { label: "Third", value: 3 },
+  { label: "Fourth", value: 4 },
+  { label: "Fifth", value: 5 },
+]
 
-  const selectLabel = "Select and options";
+function App() {
+  const [value1, setValue1] = useState<SelectOption[]>([options[0]])
 
   return (
     <>
-      <MultiSelect options={options} optionLabel={selectLabel}/>
+      <MultiSelect
+        multiple
+        options={options}
+        value={value1}
+        onChange={o => setValue1(o)}
+      />
     </>
   )
 }
--- multi-select-input/src/components/MultiSelect/index.tsx
+++ multi-select-input/src/components/MultiSelect/index.tsx
@@ -1,26 +1,151 @@
+import { useEffect, useRef, useState } from "react"
 
-type MultiSelectProps = {
-  options: SelectOption[];
-  optionLabel: string;
+export type SelectOption = {
+  label: string
+  value: string | number
 }
 
-type SelectOption = {
-  label: string;
-  value: string;
+type MultipleSelectProps = {
+  multiple: true
+  value: SelectOption[]
+  onChange: (value: SelectOption[]) => void
 }
 
-const MultiSelect = ({ options, optionLabel }: MultiSelectProps) => {
+type SingleSelectProps = {
+  multiple?: false
+  value?: SelectOption
+  onChange: (value: SelectOption | undefined) => void
+}
+
+type SelectProps = {
+  options: SelectOption[]
+} & (SingleSelectProps | MultipleSelectProps)
+
+const MultiSelect = ({ multiple, value, onChange, options }: SelectProps) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const [highlightedIndex, setHighlightedIndex] = useState<number>(0)
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  function clearOptions() {
+    multiple ? onChange([]) : onChange(undefined)
+  }
+
+  function selectOption(option: SelectOption) {
+    if (multiple) {
+      if (value.includes(option)) {
+        onChange(value.filter(o => o !== option))
+      } else {
+        onChange([...value, option])
+      }
+    } else {
+      if (option !== value) onChange(option)
+    }
+  }
+
+  function isOptionSelected(option: SelectOption) {
+    return multiple ? value.includes(option) : option === value
+  }
+
+  useEffect(() => {
+    if (isOpen) setHighlightedIndex(0)
+  }, [isOpen])
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.target != containerRef.current) return
+      switch (e.code) {
+        case "Enter":
+        case "Space":
+          setIsOpen(prev => !prev)
+          if (isOpen) selectOption(options[highlightedIndex])
+          break
+        case "ArrowUp":
+        case "ArrowDown": {
+          if (!isOpen) {
+            setIsOpen(true)
+            break
+          }
+
+          const newValue = highlightedIndex + (e.code === "ArrowDown" ? 1 : -1)
+          if (newValue >= 0 && newValue < options.length) {
+            setHighlightedIndex(newValue)
+          }
+          break
+        }
+        case "Escape":
+          setIsOpen(false)
+          break
+      }
+    }
+    containerRef.current?.addEventListener("keydown", handler)
+
+    return () => {
+      containerRef.current?.removeEventListener("keydown", handler)
+    }
+  }, [isOpen, highlightedIndex, options])
+
   return (
-    <>
-      <select>
-        <option value="">{optionLabel}</option>
-        {options?.map(({ label, value }) => {
-          return (
-            <option key={value} value={value}>{label}</option>
-          )
-        })}
-      </select>
-    </>
+    <div
+      ref={containerRef}
+      onBlur={() => setIsOpen(false)}
+      onClick={() => setIsOpen(prev => !prev)}
+      tabIndex={0}
+      className="select-container"
+    >
+      <span className="value">
+        {multiple
+          ? Array.from(new Set(value)).map(v => (
+            <button
+              key={v.value}
+              onClick={e => {
+                e.stopPropagation()
+                selectOption(v)
+              }}
+              className="option-badge"
+            >
+              {v.label}
+              <span className="remove-btn">&times;</span>
+            </button>
+          ))
+          : value?.label}
+      </span>
+
+      <div className="flex items-center gap-2 justify-start">
+        <button
+          onClick={e => {
+            e.stopPropagation()
+            clearOptions()
+          }}
+          className="clear-btn"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+
+        </button>
+        <div className="divider" />
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="caret">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+        </svg>
+      </div>
+      <ul className={`options ${isOpen ? 'show' : ""}`}>
+        {options.map((option, index) => (
+          <li
+            onClick={e => {
+              e.stopPropagation()
+              selectOption(option)
+              setIsOpen(false)
+            }}
+            onMouseEnter={() => setHighlightedIndex(index)}
+            key={option.value}
+            className={`option ${isOptionSelected(option) ? 'selected' : ""
+              } ${index === highlightedIndex ? 'highlighted' : ""}`}
+          >
+            {option.label}
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
